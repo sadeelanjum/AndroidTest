3d57de9c119ceb73d560faaab2069cc4591b7538
sadeel <sadeel094@gmail.com>
2018-04-03 17:45:26 +0500

    adding folders 1st time

---
+++ includes/nav_header.php
@@ -0,0 +1,30 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../css/design.css">
+		<link rel="stylesheet" href="../css/bootstrap.css">
+	</head>
+	
+	<body>
+		
+			<div class="jumbotron" style="padding: 2rem; height:100px; margin-bottom: 0rem">
+		
+				<div id ="logo_div">
+					<img id="logo_img" src="../img/akbc_logo.png"> </img>
+				</div>
+				
+				<div id="slogan_div">
+					<span class="slogan_text"> Shaheen Business Group</span><br>
+					<span class="slogan_text2"> Chairman: Rana Ali Shan Khan </span>				
+				</div>
+			</div>
+	
+	</body>
+	
+</html>
+	
+ 
